a575e7039306061edf63889b2f6f2cdd9f7f0565
alok@itobuz.com <alokranjanjoshi07567@gmail.com>
2024-02-19 14:38:31 +0530

    completed with the theme switch

--- main.mjs
+++ main.mjs
@@ -1,5 +1,63 @@
 const themeToggler = document.querySelector(".web-theme-toggler");
+const takeATourEmbed = document.querySelector(".youtube-group embed");
+const commaEmbeds = document.querySelectorAll(
+  ".carausel-card-body > .icon-container > embed"
+);
+const carauselCards = document.querySelectorAll(".carausel-card");
+const carauselNavigation = document.querySelector(".carausel-navigation");
+const arrowRight = document.querySelectorAll(".project-card-body > a > embed");
+console.log(arrowRight);
 
+// carausel
+const TRANSLATE_PERCENTAGE = 100;
+const INITIAL_TRANSLATE = window.innerWidth >= 992 ? 100 : 0;
+// const TRANSLATE_PERCENTAGE = window.innerWidth >= 992 ? 105 : 130;
+
+let currSlide = 0;
+let maxLength = carauselCards.length;
+let prevDot = 0;
+
+carauselCards.forEach((_, i) => {
+  carauselNavigation.insertAdjacentHTML(
+    "beforeend",
+    `<span class="carausel-dot p-1 rounded-5" data-slide=${i}>`
+  );
+});
+const goToSlide = function () {
+  carauselNavigation.children[prevDot].classList.remove("curr-active-slide");
+  carauselNavigation.children[currSlide].classList.add("curr-active-slide");
+  prevDot = currSlide;
+  carauselCards.forEach((card, i) => {
+    card.style.transform = `translateX(${
+      TRANSLATE_PERCENTAGE * (i - currSlide) + INITIAL_TRANSLATE
+    }%)`;
+  });
+};
+goToSlide(currSlide);
+
+function prevSlide() {
+  if (currSlide === 0) currSlide = maxLength - 1;
+  else currSlide--;
+  goToSlide(currSlide);
+}
+function nextSlide() {
+  if (currSlide === maxLength - 1) currSlide = 0;
+  else currSlide++;
+  goToSlide(currSlide);
+}
+
+window.addEventListener("keydown", function (e) {
+  e.key.toLowerCase() === "arrowleft" && prevSlide(currSlide);
+  e.key.toLowerCase() === "arrowright" && nextSlide();
+});
+
+carauselNavigation.addEventListener("click", function (e) {
+  if (e.target.tagName !== "SPAN") return;
+  currSlide = e.target.dataset.slide;
+  goToSlide();
+});
+
+//////// theme change
 function convertCssToArray(str) {
   let arr = str.split("\n").join("").split(";");
   arr.forEach((a, i) => {
@@ -90,21 +148,60 @@ let lightColors = `--actual-black-color: #000;
   --contact-description-color: #909090;
   --contact-input-border-color: #d0c3c9;
   --copy-right-border-color: #eae5e5;
-  --whitish-grey-color: #e9e9e9;`;
+  --whitish-grey-color: #e9e9e9;
+  `;
+
+const svgColors = {
+  light: {
+    takeATour: "#212528",
+    invertedComma: "#f4f4f4",
+    arrowRight: "#212528",
+  },
+  dark: {
+    takeATour: "#fff",
+    invertedComma: "#474242",
+    arrowRight: "#d5d9d4",
+  },
+};
 
 lightColors = convertCssToArray(lightColors);
 darkColors = convertCssToArray(darkColors);
 
-function setTheme(arr) {
+function fillSvgColor(embed, color, isFill, isStroke) {
+  embed.getSVGDocument().firstChild.childNodes.forEach((node) => {
+    if (node.tagName?.toLowerCase() === "path") {
+      isFill && node.setAttribute("fill", color);
+      if (node.getAttribute("stroke"))
+        isStroke && node.setAttribute("stroke", color);
+    }
+  });
+}
+
+function setSVGFillColor(colors) {
+  commaEmbeds.forEach((embed) =>
+    fillSvgColor(embed, colors.invertedComma, true, false)
+  );
+  fillSvgColor(takeATourEmbed, colors.takeATour, true, false);
+  arrowRight.forEach((embed) =>
+    fillSvgColor(embed, colors.arrowRight, false, true)
+  );
+}
+
+function setTheme(arr, colors) {
   arr.forEach(([propertyName, propertyValue]) =>
     document.documentElement.style.setProperty(propertyName, propertyValue)
   );
+  setSVGFillColor(colors);
 }
 
 function themeSetter(e) {
   const isDarkMode = e.target.checked;
 
-  isDarkMode ? setTheme(darkColors) : setTheme(lightColors);
+  isDarkMode
+    ? setTheme(darkColors, svgColors.dark)
+    : setTheme(lightColors, svgColors.light);
 }
 themeToggler.addEventListener("change", themeSetter);
 window.addEventListener("load", themeSetter);
+
+//////////
